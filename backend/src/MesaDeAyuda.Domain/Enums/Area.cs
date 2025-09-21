b08903d53bba89afc060db5af6f9431df5b61841
Alexis José Calderón Díaz <alexisjosecalderondiaz@gmail.com>
2025-09-21 17:12:27 -0300

    Agregar enumeración Area con las áreas del proyecto

---
+++ backend/src/MesaDeAyuda.Domain/Enums/Area.cs
@@ -0,0 +1,13 @@
+namespace MesaDeAyuda.Domain.Enums;
+
+public enum Area
+{
+    Calidad = 1,
+    Desarrollo = 2,
+    Finanzas = 3,
+    Logística = 4,
+    Mantención = 5,
+    Producción = 6,
+    RRHH = 7,
+    Ventas = 8,
+}
