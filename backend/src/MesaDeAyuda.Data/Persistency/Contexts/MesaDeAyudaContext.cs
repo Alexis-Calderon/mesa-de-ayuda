de0aab2ff276932031d27fb82879cb8a79c9f43b
Alexis José Calderón Díaz <alexisjosecalderondiaz@gmail.com>
2025-09-21 17:13:20 -0300

    Agregar contexto de base de datos MesaDeAyuda con DbSet para Usuarios y Tickets

---
+++ backend/src/MesaDeAyuda.Data/Persistency/Contexts/MesaDeAyudaContext.cs
@@ -0,0 +1,10 @@
+using MesaDeAyuda.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MesaDeAyuda.Data.Persistency.Contexts;
+
+public class MesaDeAyudaContext(DbContextOptions<MesaDeAyudaContext> options) : DbContext(options)
+{
+    public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<Ticket> Tickets { get; set; }
+}
