fbc021a854ecfd9b09b07c61eccf5aa0c7998222
Alexis José Calderón Díaz <alexisjosecalderondiaz@gmail.com>
2025-09-21 18:52:47 -0300

    Agregar columnas Contrasenia y FechaCreacion a la tabla Usuario en la migración

--- backend/src/MesaDeAyuda.Data/Persistency/Migrations/20250921214356_AddContraseniaAndFechaCreado.Designer.cs
+++ backend/src/MesaDeAyuda.Data/Persistency/Migrations/20250921214356_AddContraseniaAndFechaCreado.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using MesaDeAyuda.Data.Persistency.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MesaDeAyuda.Data.Persistency.Migrations
 {
     [DbContext(typeof(MesaDeAyudaContext))]
-    partial class MesaDeAyudaContextModelSnapshot : ModelSnapshot
+    [Migration("20250921214356_AddContraseniaAndFechaCreado")]
+    partial class AddContraseniaAndFechaCreado
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.9");
---
+++ backend/src/MesaDeAyuda.Data/Persistency/Migrations/20250921214356_AddContraseniaAndFechaCreado.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MesaDeAyuda.Data.Persistency.Migrations;
+
+/// <inheritdoc />
+public partial class AddContraseniaAndFechaCreado : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<string>(
+            name: "Contrasenia",
+            table: "Usuario",
+            type: "TEXT",
+            maxLength: 100,
+            schema: "MesaDeAyuda",
+            nullable: true
+        );
+
+        migrationBuilder.AddColumn<DateTime>(
+            name: "FechaCreacion",
+            table: "Usuario",
+            type: "TEXT",
+            schema: "MesaDeAyuda",
+            nullable: false,
+            defaultValue: new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc)
+        );
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(name: "Contrasenia", table: "Usuario", schema: "MesaDeAyuda");
+
+        migrationBuilder.DropColumn(name: "FechaCreacion", table: "Usuario", schema: "MesaDeAyuda");
+    }
+}
